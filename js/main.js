838f311dac734adab8432784cc4e4c6a0bcd15de
devmamunbd <itsmemamunbd@gmail.com>
2025-04-25 18:07:28 +0600

    navbar responsive done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// responsive navbar
+const menuBtn = document.querySelector(".mobile-menu ul");
+const button = document.querySelector(".header-buttons-mobile");
+const bars = document.getElementById("bar");
+bars.addEventListener("click", () => {
+  // toggle the menu and button classes to show/hide the menu
+  menuBtn.classList.toggle("active");
+  button.classList.toggle("active");
+
+  // toggle the button icon between bars and x
+  if (menuBtn.classList.contains("active")) {
+    bars.classList.remove("fa-bars");
+    bars.classList.add("fa-xmark");
+  } else {
+    bars.classList.remove("fa-xmark");
+    bars.classList.add("fa-bars");
+  }
+});
+
 // according to the FAQ section
 const faqs = document.querySelectorAll(".faq");
 const title = document.querySelector(".faq-title");
